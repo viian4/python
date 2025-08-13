b4e6d6b2c6f0a78cb331cdf87f80fb1454ed427b
viian4 <vianaeu7@gmail.com>
2025-08-12 21:35:24 -0400

    Adiciona desafios 24 a 27 e atualiza desafio 23

--- aula9/desafio23.py
+++ aula9/desafio23.py
@@ -1,1 +1,10 @@
 num = int(input('Informe um numero: '))
+U = num // 1 % 10
+D= num // 10 % 10
+C = num // 100 % 10
+M = num // 1000 % 10
+print(f'Analisando o numero {num} ')
+print(f'Unidade {U}')
+print(f'Dezena {D}')
+print(f'Centena {C}')
+print(f'Milhar {M}')
