5f9292072664e14ababa4f613fdeb0e16e901d70
viian4 <vianaeu7@gmail.com>
2025-09-22 21:08:43 -0400

    comentarios

--- aula13(for)/desafio56.py
+++ aula13(for)/desafio56.py
@@ -3,12 +3,17 @@ mediaidade = 0
 maioridadehomem = 0
 nomevelho = ''
 totmulher20 = 0
+'''somaidade: vai somar todas as idades para calcular a média.
+   maioridadehomem: guarda a maior idade entre os homens (começa em 0).
+   nomevelho: guarda o nome do homem mais velho (começa vazio).
+    totmulher20: contador de mulheres com menos de 20 anos.'''
 for p in range(1,5):
     print(f'_____{p} PESSOA_____')
     nome = str(input('NOME: ')).strip()
     idade = int(input('IDADE: '))
     sexo = str(input('SEXO [M/F]: ')).strip()
     somaidade += idade
+    '''Soma a idade atual à somaidade (equivalente a somaidade = somaidade + idade).'''
     if p == 1 and sexo in "Mm":
         maioridadehomem = idade
         nomevelho = nome
