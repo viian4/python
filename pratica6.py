fa442d3770eba4604bd76e5adc73cee8e4de84f9
Viana <vianaeu7@gmail.com>
2026-01-05 20:55:06 -0400

    atividade 65

---
+++ pratica6.py
@@ -0,0 +1,7 @@
+#validador de senha
+senha_correta = "HarryP0tter123"
+senha = input('Digite sua senha: ')
+while senha != senha_correta:
+    print("tente novamente123 ")
+    senha = input('digite sua senha: ')
+print('Acesso Liberado')
---
+++ pratica8.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número para a sua tabuada: '))
+c = 1
+while c <= 10 :
+    print(f"{n} x {c} = {n * c}")
+    c += 1
